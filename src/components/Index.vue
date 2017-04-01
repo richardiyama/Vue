fcec244d777bf2d472d4a7990e734cdc03cce758
Richard_Iyama_Oluwaseun <richard.iyama@orbsolutions.com.ng>
2017-04-01 20:58:54 +0100

    commit

---
+++ src/components/Index.vue
@@ -0,0 +1,130 @@
+<template>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="1">
+       Orb Solution Todo List
+      </q-toolbar-title>
+      <button @click="add">
+        <i>note_add</i>
+      </button>
+    </div>
+
+    <div class="layout-view">
+
+      <q-search v-model="searchModel"></q-search>
+      <div>{{recCount}}</div>
+      <div class="list">
+        <div :id="'test'+item._id" v-for="(item, index) in filteredTasks()" class="item">
+
+          <i class="item-primary">lightbulb_outline</i>
+          <div class="item-content has-secondary">
+            <div>{{item.label}}</div>
+          </div>
+
+          
+          <div class="item-secondary">
+            <i slot="target">
+              more_vert
+              <q-popover  :ref="'popover'">
+                <div class="list">
+                  <div class="item item-link" @click="deleteItem(index)">
+                    <div class="item-content">Delete</div>
+                  </div>
+                </div>
+              </q-popover>
+            </i>
+          </div>
+
+
+
+        </div>
+      
+      </div>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+  import _ from 'lodash'
+  import Quasar, { Utils, Dialog, LocalStorage } from 'quasar'
+
+  export default {
+    data() {
+      return {
+        searchModel: "",
+        recCount: 0,
+        
+      }
+    },
+
+    
+    methods: {
+      filteredTasks() {
+        var tasks = LocalStorage.get.item('taskList')
+        if (this.searchModel.length > 0) {
+          console.log("hello")
+          retv = _.filter(tasks, {label: this.searchModel})
+          this.recCount = retv.length
+
+          return retv
+        }
+
+        console.log("yello")
+
+        this.recCount = tasks.length
+        return tasks
+      },
+
+      add() {
+        var self = this
+        Dialog.create({
+          title: 'New task',
+          form: {
+            task: {
+              type: 'textbox',
+              label: 'My task',
+              model: ''
+            }
+          },
+         
+          buttons: [
+            'Delete',
+            {
+              label: 'Create',
+              handler(data) {
+                console.log(data)
+                var newTask = {label: data.task}
+
+                 var tasks = LocalStorage.get.item('taskList')
+                 console.log(tasks)
+                 datas.push(newTask)
+               
+                LocalStorage.set('taskList', tasks)
+                self.recCount += 1
+              }
+            }
+          ]
+        })
+      },
+       
+    
+
+      deleteItem(index) {
+
+        this.$refs.popover[0].close(index);
+        LocalStorage.get.item(index)
+        console.log(index);
+        LocalStorage.remove(index)
+        LocalStorage.set('taskList', index)
+      },
+    },
+
+    mounted() {
+      // create task store if not exist
+      if (LocalStorage.has('taskList') === false) {
+        LocalStorage.set('taskList', [])
+      }
+    }
+  }
+  
+</script>
